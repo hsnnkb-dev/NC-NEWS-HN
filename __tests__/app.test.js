b9ec7d38a401f23a6d09b1a8008ac1eb26bb1be8
Hasan N <hsnnkb.dev@gmail.com>
2022-12-12 11:49:59 +0000

    Install supertest and express, setup app.test.js and app.js

---
+++ __tests__/app.test.js
@@ -0,0 +1,10 @@
+const app = require('../app');
+const request = require('supertest');
+
+describe('api', () => {
+  describe('GET /api/topics', () => {
+    test('status: 200, returns an array of topic objects', () => {
+      
+    })
+  });
+})
