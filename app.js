85ead2bea0672427b48073d84a81900c0d28b8f2
Hasan N <hsnnkb.dev@gmail.com>
2022-12-13 10:43:30 +0000

    Create GET comments functionality and tests, amend GET articles functionality to include articles without comments

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getTopics, getArticles, getArticleById } = require('./controllers/endpoint-controllers');
+const { getTopics, getArticles, getArticleById, getCommentsByArticleId } = require('./controllers/endpoint-controllers');
 const { handleServerErrors, handle404Errors, handlePsqlErrors } = require('./controllers/error-controllers')
 
 const app = express();
 app.get('/api/topics', getTopics);
 app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id', getArticleById);
+app.get('/api/articles/:article_id/comments', getCommentsByArticleId)
 
 app.all('*', handle404Errors);
 app.use(handlePsqlErrors);
