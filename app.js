d861f32af7d6087dcac27fc7db3287cc9ae6ca57
Hasan N <hsnnkb.dev@gmail.com>
2023-01-09 10:54:16 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const {
   handleCustomErrors 
 } = require('./controllers/error-controllers');
 const apiRouter = require('./routes/api-router');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 /* ENDPOINT ROUTING */
